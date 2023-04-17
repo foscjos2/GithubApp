7c51333f7ea2734c6075127f94fb6ac20ead77e4
foscjos2 <Joshua.Foschi@episerver.com>
2023-04-17 15:19:31 -0500

    Initial commit

---
+++ GithubApp/GithubApp/Controllers/GithubAppController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GithubApp.Controllers;
+
+[ApiController]
+[Route("api/webhook")]
+public class GithubAppController
+{
+    public GithubAppController() { }
+
+    [HttpGet]
+    public string Get()
+    {
+        return "Hello world";
+    }
+}
